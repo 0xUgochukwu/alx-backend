3fc159d1a82479753341ac11d17425e5d0d46ac9
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-03 18:43:21 +0100

    fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,11 +8,20 @@ class FIFOCache(BaseCaching):
     """ First In First Out Caching Class
     """
 
+    def __init__(self):
+        super().__init__()
+        self.__queue = []
+
     def put(self, key, item):
         """ Put an item in the cache
         """
         if key and item:
-            self.cache_data[key] = item
+            self.cache_data.update({key: item})
+            self.__queue.append(key)
+            if len(self.cache_data) > self.MAX_ITEMS:
+                first_in = self.__queue.pop(0)
+                self.cache_data.pop(first_in)
+                print(f'DISCARD: {first_in}')
 
     def get(self, key):
         """ Get an item from the cache
