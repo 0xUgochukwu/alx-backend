dd3abd562803013f1490a81ff186fa30fd1d9b50
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-03 20:41:44 +0100

    lfu

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Caching Implementation.
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ Least Frequently Used Caching Class
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.__queue = [] 
+        self.__trackpad = {}
+
+    def put(self, key, item):
+        """ Put an item in the cache
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.__trackpad[key] += 1
+                self.__queue.remove(key)
+            else:
+                self.__trackpad[key] = 1
+            
+            self.cache_data[key] = item
+            self.__queue.append(key)
+
+            if len(self.cache_data) > self.MAX_ITEMS:
+                least_f_used = min(self.__trackpad,key=lambda k: (
+                    self.__trackpad[k], self.__queue.index(k)))
+                self.__trackpad.pop(least_f_used)
+                self.__queue.remove(least_f_used)
+                self.cache_data.pop(least_f_used)
+                print(f'DISCARD: {least_f_used}')
+                # print(self.__trackpad)
+                # print(self.__queue)
+                # print(self.cache_data)
+
+    def get(self, key):
+        """ Get an item from the cache
+        """
+        if key in self.cache_data:
+            self.__trackpad[key] += 1
+            self.__queue.remove(key)
+            self.__queue.append(key)
+        
+        return self.cache_data.get(key, None)
