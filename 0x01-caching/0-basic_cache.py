ef01e5a446318634ea46a9c8571acc41c41ef6fe
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-03 18:09:33 +0100

    put & get

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,21 +1,23 @@
-#!/usr/bin/env python3
-""" Caching
+#!/usr/bin/python3
+"""Basic Cache implementation Class
 """
-
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Basic Caching Class
     """
+    A basic cache implementaion class
 
+    Attributes:
+        MAX_ITEMS: number of items that can be store in the cache
+    """
     def put(self, key, item):
-        """ Put an item in the cache
+        """ Add an item in the cache
         """
         if key is not None and item is not None:
-            self.cache_data[key] = item
+            self.cache_data.update({key: item})
 
     def get(self, key):
-        """ Get an item from the cache
+        """ Get an item by key
         """
         return self.cache_data.get(key, None)
