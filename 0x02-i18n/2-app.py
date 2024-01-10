2205366842f43a0cb381e2e41cb66aba5d6c35cf
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-10 03:59:22 +0100

    get_locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+''' i18n - Basic Flask App
+'''
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_babel import Babel
+
+
+class Config(object):
+    ''' App Configuration class
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    ''' Gets the locale language from request
+    '''
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    ''' Index route
+    '''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
