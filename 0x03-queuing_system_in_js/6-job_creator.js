9ddbcb8429a7cce160eaed74c81e3116241fa4c8
0xUgochukwu <0xUgochukwu@gmail.com>
2024-02-07 16:01:13 +0100

    jobs

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -9,13 +9,12 @@ const data = {
 
 const job = queue
   .create('push_notification_code', data)
+  .on('complete', () => {
+    console.log("Notification job completed")
+  })
+  .on('failed', () => {
+    console.log("Notification job failed")
+  })
   .save((err) => {
     if (!err) console.log("Notification job created:", job.id);
   });
-
-job.on('completed', () => {
-  console.log("Notification job completed")
-});
-job.on('failed', () => {
-  console.log("Notification job failed")
-});
